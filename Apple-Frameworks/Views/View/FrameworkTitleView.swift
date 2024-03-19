d34ff7618d75b700d6f715ba74b860607bc380d5
YaninaKo <yanina.kovrakh@gmail.com>
2024-03-19 15:35:48 +0100

    create groups

---
+++ Apple-Frameworks/Views/View/FrameworkTitleView.swift
@@ -0,0 +1,33 @@
+//
+//  FrameworkTitleView.swift
+//  Apple-Frameworks
+//
+//  Created by Yanina Kovrakh on 19.03.2024.
+//
+
+import SwiftUI
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height:  90)
+                .aspectRatio(contentMode: .fit)
+                
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FrameworkTitleView(framework: MockData.sampleFramework)
+}
